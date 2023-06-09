c57afcceacafb65ea2bac0109b073e2f7ecf6a20
Moetasim97 <valencian97@gmail.com>
2023-06-09 15:58:11 +0300

    all added

---
+++ first_app/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Currently from "./currently_reading.js"
+import React from 'react';
+
+
+class App extends React.Component {
+  constructor(props){
+   
+    this.state={nam:"whatever"}
+  }
+  super(props)
+  
+  
+
+  render(){
+  
+    return(
+    <div>Just a single div</div>
+    )
+  };
+
+  
+  
+}
+
+export default App;
